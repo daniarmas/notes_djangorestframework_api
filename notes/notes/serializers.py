32b4a588e5ed6f82bb24d971ac78d5073c69952b
Daniel Armas <daniel.armas9706@gmail.com>
2022-10-28 15:12:30 -0400

    Implementing the GroupNoteSerializer.

--- notes/notes/serializers.py
+++ notes/notes/serializers.py
@@ -12,3 +12,23 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class GroupNoteSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(
+        required=True, allow_blank=False, max_length=100)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Group` instance, given the validated data.
+        """
+        return Group.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Group` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.save()
+        return instance
